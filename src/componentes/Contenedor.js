a3e0c0298bf0402a950602ebaa70f0585ea1a8b7
julysa <jmcadillo.06@gmail.com>
2019-06-08 17:31:38 -0500

    iniciando

---
+++ src/componentes/Contenedor.js
@@ -0,0 +1,21 @@
+import React from "react";
+import SaborHelados from "./SaborHelados";
+import PrecioHelados from "./PrecioHelados";
+
+
+const Contenedor = () => (
+ <div className="contenedor">
+    <div> Linea de Tiempo</div>
+    <div className="contenido">   
+        <SaborHelados></SaborHelados>
+        <PrecioHelados></PrecioHelados>
+        </div>
+     <div> 
+         
+         </div>   
+    </div>
+
+
+);
+  
+export default Contenedor;
---
+++ src/componentes/PrecioHelados.js
@@ -0,0 +1,10 @@
+import React from "react";
+/**
+ * Luigi
+ */
+const PrecioHelados=()=>(
+    <div className="preciohelados">Precios de los Helados<br/>  <br/>
+    </div>
+    
+);
+export default  ClimaDetalle;
